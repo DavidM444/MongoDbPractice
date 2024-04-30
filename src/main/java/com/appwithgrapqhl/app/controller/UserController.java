5be73bb40c4b0d541ee2597032a4dd3622c789cc
DavidM444 <david541gamer@gmail.com>
2024-04-29 19:28:03 -0500

    Controller user creation.

---
+++ src/main/java/com/appwithgrapqhl/app/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.appwithgrapqhl.app.controller;
+
+import com.appwithgrapqhl.app.Entiy.UserData;
+import com.appwithgrapqhl.app.Entiy.User;
+import com.appwithgrapqhl.app.resolvers.UserResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class UserController {
+
+    private final UserResolver userResolver;
+
+    @Autowired
+    public UserController(UserResolver userResolver){
+        this.userResolver = userResolver;
+    }
+
+
+    @MutationMapping
+    public Mono<User> saveUser(@Argument String name, @Argument String email, @Argument Integer edad){
+        UserData data = new UserData(name,email,edad);
+        return userResolver.saveUserResolver(data);
+    }
+
+    @QueryMapping
+    public Mono<User> findUserById(@Argument String  id){
+        return userResolver.getUserResolver(id);
+    }
+
+    @QueryMapping
+    public Flux<User> findAllUsers(){
+        return userResolver.allUsersResolver();
+    }
+}
