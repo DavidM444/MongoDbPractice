3c3708cf97dbb44eb84c01c4895c6c4eed3db1a5
DavidM444 <david541gamer@gmail.com>
2024-04-30 20:06:05 -0500

    change name document.

--- src/main/java/com/appwithgrapqhl/app/Entiy/User.java
+++ src/main/java/com/appwithgrapqhl/app/Entiy/User.java
@@ -1,11 +1,7 @@
 package com.appwithgrapqhl.app.Entiy;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.UUID;
-
-@Document
+@Document(collection = "users")
 public class User {
     @Id
     private String id;
