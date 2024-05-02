a243594cbd254c0ca58ee5d009e887009d0bbdd8
DavidM444 <david541gamer@gmail.com>
2024-05-01 20:25:58 -0500

    test classes for jar.

--- src/test/java/com/appwithgrapqhl/mongoapp/MongoappApplicationTests.java
+++ src/test/java/com/appwithgrapqhl/mongoapp/MongoappApplicationTests.java
@@ -1,13 +1,15 @@
 package com.appwithgrapqhl.mongoapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.ContextIdApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MongoappApplicationTests.class)
 class MongoappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("contexto cargado");
 	}
 
 }
